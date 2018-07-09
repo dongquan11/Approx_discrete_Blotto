5a53412d90b37d869a2f861c1526f8cfb3ebd8a1
dongquan11 <38979946+dongquan11@users.noreply.github.com>
2018-07-09 20:54:56 +0200

    Update BF values.R

--- BF values.R
+++ BF values.R
@@ -1,8 +1,8 @@
 #Create/read the values of the battlefields.
 create_value <- function (){
-    v <<- runif(N, v_min, v_max)                #
-    time <- gsub(":","",as.character(Sys.time()))
-    write.csv(c(N,m,p,v), "values_latest.csv")  #write down the vector into the file to use later
+    v <<- runif(N, v_min, v_max)                # Generate a vector of values uniformly from [v_min, v_max]
+    time <- gsub(":","",as.character(Sys.time())) 
+    write.csv(c(N,m,p,v), "values_latest.csv")  # write down the vector into the file to use later
 }
 
 read_value <- function(){
